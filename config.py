1475f541d63c369fecba594822dd54495268f1b0
Gittechie111 <trizamuseve@gmail.com>
2026-03-12 14:21:09 +0300

    Added databse configuration

--- config.py
+++ config.py
@@ -1,7 +1,15 @@
 import os
 from dotenv import load_dotenv
 
+#load environment variables from .env file
 load_dotenv()
+
+class Config:
+    # Database configuration
+    DB_USER = os.getenv('DB_USER')
+    DB_PASSWORD = os.getenv('DB_PASSWORD')
+    DB_HOST = os.getenv('DB_HOST')
+    DB_NAME = os.getenv('DB_NAME')
 
 class Config:
     SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}"
